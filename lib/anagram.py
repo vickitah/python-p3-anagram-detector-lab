5e6c3019a5e4f543bf8d4c7f76a43e52a9de45b6
victor <victor.mutuma@student.moringaschool.com>
2025-05-12 10:49:26 +0300

    save changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            if candidate.lower() != self.word:
+                if sorted(candidate.lower()) == self.sorted_word:
+                    matches.append(candidate)
+        return matches
